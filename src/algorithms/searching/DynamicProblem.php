022c4fac01468fe8901a38ad9c96bd72cddd7b23
Surajudeen Akande <surajudeen.akande@fire-engine-red.com>
2017-07-26 21:12:06 +0100

    Fix bug

--- src/algorithms/searching/DynamicProblem.php
+++ src/algorithms/searching/DynamicProblem.php
@@ -5,11 +5,11 @@ function DPFibonacci(int $n)
   $fib = [];
   $fib[0] = 0; $fib[1] = 1;
 
-  for ($i = 2; $i < $n; $i++) {
+  for ($i = 2; $i <= $n; $i++) {
     $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
   }
 
-  return $fib[$n - 1];
+  return $fib[$n];
 }
 
-print_r(DPFibonacci(10));
+print_r(DPFibonacci(2));
